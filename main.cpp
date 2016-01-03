449231f3b7f4419ee63e0c347cfa197e2fadcd78
gerard <gklomphaar@hotmail.com>
2016-01-03 17:20:39 +0100

    Started on testing, - Implemented basic sorting interface

--- main.cpp
+++ main.cpp
@@ -6,51 +6,43 @@
 #include "TestCases.h"
 #include "Sorting.h"
 
+
+/***********************
+ * Main function of this file:
+ * - Testing efficiencies of different sorting algorithms.
+ * Secondary intentions:
+ * - Practise implementing different sorting strategies
+ * - Practise on C++ templating
+ * - Practise design patterns.
+ *************************/
+
 using namespace std;
 
 // Create an interface for sorting methods
+template<typename ArrayType>
 class SortMethod
 {
 public:
-    virtual void executeSort() = 0;
+    virtual void executeSort(ArrayType* data, int dataSize) = 0;
 };
-
-template <typename T>
-struct DataSpecification
+template<typename ArrayType>
+class MergeSort : public SortMethod<ArrayType>
 {
-    T* pData;
-    int dataSize;
-    DataSpecification(T pData, int dataSize): pData(pData), dataSize(dataSize){}
-};
-
-// Implementation of sorting methods
-class MergeSort : public SortMethod
-{
-public:
-    void executeSort()
+    virtual void executeSort(ArrayType* data, int dataSize)
     {
-        assert(m_dataType.pData);
-        Sorting::mergeSort(m_dataType.pData, m_dataType.dataSize);
-    }
-
-    void setDataType(DataSpecification dataType)
-    {
-        m_dataType = dataType;
-    }
-private:
-    DataSpecification m_dataType;
-};
-/*
-class SelectionSort : public SortMethod
-{
-public:
-    template <typename T, T pData, int dataSize>
-    void executeSort()
-    {
-        Sorting::selectionSort(pData,dataSize);
+        Sorting::mergeSort(data, dataSize);
     }
 };
-*/
+template<typename ArrayType>
+class SelectionSort : public SortMethod<ArrayType>
+{
+    virtual void executeSort(ArrayType* data, int dataSize)
+    {
+        Sorting::selectionSort(data, dataSize);
+    }
+};
+
+
 // Use the same array with different sorting techniques.
 // Possible with different data types
 template<typename ArrayType>
@@ -62,7 +54,7 @@ public:
     {
         m_TestArray.reset(new ArrayType[m_testArraySize]);
     }
-    void startMeasurement(SortMethod* testMethod)
+    void startSortMeasurement(SortMethod<ArrayType>& testMethod)
     {
         // Create a new array, and copy the content of the testArray.
         unique_ptr<ArrayType> tempArray(new ArrayType[m_testArraySize]);
@@ -70,8 +62,7 @@ public:
 
         // Do the test.
         auto startTime = chrono::system_clock::now();
-        //Sorting::mergeSort(m_TestArray.get(), m_testArraySize);
-        //testMethod->executeSort(m_TestArray.get(), m_testArraySize);
+        testMethod.executeSort(m_TestArray.get(), m_testArraySize);
         auto endTime = chrono::system_clock::now();
 
         // Print result.
@@ -93,13 +84,14 @@ int main() {
     cout << endl;
 
     // Create the sorting methods/interfaces.
-    MergeSort<> mergeSort;
-    //SelectionSort selectionSort;
+    MergeSort<char> mergeSort;
+    SelectionSort<char> selectionSort;
 
     // Testing
     cout << "Running the measurements: " << endl;
     TestSorting<char> testCharSort(10000);
-    //testCharSort.startMeasurement(selectionSort);
+    testCharSort.startSortMeasurement(selectionSort);
+    testCharSort.startSortMeasurement(mergeSort);
 
     if(0){
         const int arraySize = 100000;
