a63a1444603c1f2e77b7317cf7b4cb6778e8aa01
SupaGait <gklomphaar@gmail.com>
2016-02-21 18:54:14 +0100

    Cleaned up a bit - Testing with base interface so can be used on array and list

---
+++ Lists/LinkedList.h
@@ -0,0 +1,13 @@
+//
+// Created by Gerard on 21-2-2016.
+//
+
+#ifndef ALGORITHMS_LINKEDLIST_H
+#define ALGORITHMS_LINKEDLIST_H
+
+class LinkedList
+{
+
+};
+
+#endif //ALGORITHMS_LINKEDLIST_H
--- Lists/main.cpp
+++ Lists/main.cpp
@@ -7,59 +7,74 @@
 #include "SomeClass.h"
 #include "ArrayList.h"
 
+void testArrayList();
+
+// Prototypes
+void testGet(List<SomeClass> &&list);
+void testAdd(List<SomeClass> &&list);
+void testRemove(List<SomeClass> &&list);
+
 using std::cout;
 using std::endl;
 
 int main()
 {
-    {
-        cout << endl << "Test out of range exception on getAt()." << endl;
-        SomeClass::resetCounter();
-
-        ArrayList<SomeClass> arrayList; // Empty arrayList
-
-        try {
-            cout << arrayList.getAt(1).getName() << endl;
-        }
-        catch (const std::exception e) {
-            cout << "Ow damm.. there was an exception (Yes, this is actually good) : " << e.what() << endl;
-        }
-    }
-
-    {
-        cout << endl << "Test adding elements and re-allocation." << endl;
-        SomeClass::resetCounter();
-
-        ArrayList<SomeClass> arrayList;
-        printListElements(arrayList);
-
-        cout << endl;
-        SomeClass sjon = SomeClass("Sjon1");
-        arrayList.add( sjon );
-        arrayList.add( SomeClass("Sjaak2"));
-        printListElements(arrayList);
-
-        cout << endl;
-        arrayList.add( SomeClass("Henk3") );
-        arrayList.add( SomeClass("Piet4") );
-        printListElements(arrayList);
-    }
-
-    {
-        cout << endl << "Test removing elements." << endl;
-        SomeClass::resetCounter();
-
-        ArrayList<SomeClass> arrayList;
-        arrayList.add( SomeClass("PersonPos0") );
-        arrayList.add( SomeClass("PersonPos1") );
-        arrayList.add( SomeClass("PersonPos2") );
-        arrayList.add( SomeClass("PersonPos3 < delete") );
-        arrayList.add( SomeClass("PersonPos4") );
-        arrayList.add( SomeClass("PersonPos5") );
-        printListElements(arrayList);
-
-        arrayList.remove(3);
-        printListElements(arrayList);
-    }
+    testArrayList();
 }
+
+void testArrayList() {
+
+    // Test functionality of arraylist
+    // Each function receives a new instance
+    testGet( ArrayList<SomeClass>() );
+    testAdd( ArrayList<SomeClass>() );
+    testRemove( ArrayList<SomeClass>() );
+
+}
+
+void testRemove(List<SomeClass> &&list) {
+    cout << endl << "Test removing elements." << endl;
+    SomeClass::resetCounter();
+
+    list.add( SomeClass("PersonPos0") );
+    list.add( SomeClass("PersonPos1") );
+    list.add( SomeClass("PersonPos2") );
+    list.add( SomeClass("PersonPos3 < delete") );
+    list.add( SomeClass("PersonPos4") );
+    list.add( SomeClass("PersonPos5") );
+    printListElements(list);
+
+    list.remove(3);
+    printListElements(list);
+}
+
+void testAdd(List<SomeClass> &&list) {
+    cout << endl << "Test adding elements and re-allocation." << endl;
+    SomeClass::resetCounter();
+
+    printListElements(list);
+
+    cout << endl;
+    SomeClass sjon = SomeClass("Sjon1");
+    list.add( sjon );
+    list.add( SomeClass("Sjaak2"));
+    printListElements(list);
+
+    cout << endl;
+    list.add( SomeClass("Henk3") );
+    list.add( SomeClass("Piet4") );
+    printListElements(list);
+}
+
+void testGet(List<SomeClass> &&list) {
+    cout << endl << "Test out of range exception on getAt()." << endl;
+    SomeClass::resetCounter();
+
+    try {
+        cout << list.getAt(1).getName() << endl;
+    }
+    catch (const std::exception e) {
+        cout << "Ow damm.. there was an exception (Yes, this is actually good) : " << e.what() << endl;
+    }
+   }
 
